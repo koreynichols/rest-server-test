4b16569e93b6bfd0ae8ff475776fc95c5f1eb543
Korey Nichols <koreynichols1@gmail.com>
2021-03-07 14:09:12 -0800

    added permission classes to truck and boat views

--- vehicle/boat/views.py
+++ vehicle/boat/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from .serializers import Boat_Serializer
 from .models import Boat
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
 
 class Boat_Viewset(viewsets.ModelViewSet):
     queryset = Boat.objects.all()
     serializer_class = Boat_Serializer
+        permission_classes = (IsAuthenticated, )
--- vehicle/truck/views.py
+++ vehicle/truck/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from .serializers import Truck_Serializer
 from .models import Truck
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
 
 class Truck_Viewset(viewsets.ModelViewSet):
     queryset = Truck.objects.all()
     serializer_class = Truck_Serializer
+    permission_classes = (IsAuthenticated, )

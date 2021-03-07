c4e60b5fb9c7e65691b00b481a73b133515d3d79
Korey Nichols <koreynichols1@gmail.com>
2021-03-06 16:54:07 -0800

    added truck viewset

--- vehicle/truck/views.py
+++ vehicle/truck/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import Truck_Serializer
+from .models import Truck
 # Create your views here.
+
+class Truck_Viewset(viewsets.ModelViewSet):
+    queryset = Truck.objects.all()
+    serializer_class = Truck_Serializer
